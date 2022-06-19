a4b84019658dfd5c01b9e9bfce2886d17bfa4723
icequeen101 <43222443+icequeen101@users.noreply.github.com>
2022-06-18 20:31:15 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def max_sub_array_of_size_k(k, arr):
+    # TODO: Write your code here
+
+    # Verification
+    # for i in arr:
+    #     print(i)
+
+    # Brute Force
+    sum = 0
+    for (i = 0; i <= 5; i++):
+
+    return -1
+
+
+max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2]);
